193be1d5559808639ea3b9bd9fd783db75ab5c81
connornlucas <connor_n_lucas@hotmail.com>
2024-09-28 16:57:50 -0700

    Added Assertation Pass/Fail conditons and console.logs

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log('Assertion Passed: ' + actual + " === " + expected);
+  }
+  else console.log('Assertion Failed: ' + actual + ' !== ' + expected)
+};
+
+//Test
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
