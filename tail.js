eff9c9ea2d1c11c15784dce15850f8fe3510cfe2
connornlucas <connor_n_lucas@hotmail.com>
2024-09-29 10:45:32 -0700

    created tail.js and wrote code to return an array excluding the first element

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔺 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// We want to create a function tail, that returns the tail (every element excluding the first) as a new array, 
+
+const tail = function (array) {
+  return array.slice(1);
+};
+
+// Tests
+assertEqual(tail([5, 6]), 6);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(tail([1,5]),);
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
