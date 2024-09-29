d85c725ef532cc9a370def7be1627171e5ec4251
connornlucas <connor_n_lucas@hotmail.com>
2024-09-28 17:53:15 -0700

    Added template literals and cleaned up the code by removing excess concatination

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🟩 Assertion Passed: ' + actual + " === " + expected);
-  } else console.log('🔺 Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔺 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 //Test
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1, 1);
+assertEqual("Connor", "Connor");
