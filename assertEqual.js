53e786a060240d2f60472f93ffc5930dc2fa5820
connornlucas <connor_n_lucas@hotmail.com>
2024-09-28 17:06:40 -0700

    added emojis to make it more obvious if the function produced a pass or fail

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('Assertion Passed: ' + actual + " === " + expected);
-  } else console.log('Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log('🟩 Assertion Passed: ' + actual + " === " + expected);
+  } else console.log('🔺 Assertion Failed: ' + actual + ' !== ' + expected);
 };
 
 //Test
