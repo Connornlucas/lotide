b0e3c0ac681166a53926ad527c276a4f6633cff2
connornlucas <connor_n_lucas@hotmail.com>
2024-09-28 17:01:30 -0700

    Used eslint to clean up code, and confirmed with tests that code is working as intended.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,7 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log('Assertion Passed: ' + actual + " === " + expected);
-  }
-  else console.log('Assertion Failed: ' + actual + ' !== ' + expected)
+  } else console.log('Assertion Failed: ' + actual + ' !== ' + expected);
 };
 
 //Test
