e9291c00ca0b7de45a1cbc65a5c601f43d2c45e9
connornlucas <connor_n_lucas@hotmail.com>
2024-09-29 11:57:05 -0700

    commented out the tests

--- tail.js
+++ tail.js
@@ -12,12 +12,17 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-// Tests
-assertEqual(tail([5, 6]), 6);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail([1,5]),);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// // Tests
+// assertEqual(tail([5, 6]), 6);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(tail([1,5]),);
+// 
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// 
+// 
+// let other = tail([5, 6]); 
+// assertEqual(other[0], 6);
+// assertEqual(other, 6);
