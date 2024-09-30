212bc94d2e5357506a13226d8e15b9c776a8de6b
connornlucas <connor_n_lucas@hotmail.com>
2024-09-30 15:55:41 -0700

    changed console.log to return and added more tests

--- without.js
+++ without.js
@@ -29,7 +29,7 @@ const without = function(array1, array2) {
       withoutArray.push(element);
     }
   }
-  console.log(withoutArray);
+  return withoutArray;
 };
 
 without([1, 2, 3], [1]); // => [2, 3]
@@ -39,3 +39,5 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => should PASS
