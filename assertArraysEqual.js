3f5ee3bdc3d77fce906ba1f39a19ea0bf3714ce9
connornlucas <connor_n_lucas@hotmail.com>
2024-09-30 15:11:00 -0700

    Cleaned up with eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,19 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`🟢 Assertion Passed: ${array1} and ${array2} are equal.`);
-  }
-  else {
+  } else {
     console.log(`🔻 Assertion Failed: ${array1} and ${array2} are not equal.`);
   }
 };
