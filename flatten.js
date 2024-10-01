427aa5524c9155a9eedd4dcc8613dc5ff95d82a5
connornlucas <connor_n_lucas@hotmail.com>
2024-10-01 12:05:06 -0700

    Linted and removed else as it was redundant

--- flatten.js
+++ flatten.js
@@ -23,16 +23,15 @@ const assertArraysEqual = function(array1, array2) {
 // We can youse the function Array.isArray() to help us determine
 
 const flatten = function(array) {
-  let flatArray = []
+  let flatArray = [];
   array.forEach(function(element, index) {
     if (Array.isArray(element)) {
       element.forEach(function(element2) {
-        flatArray.push(element2)
-      })
-    }
-    else flatArray.push(element)
-  })
-  return flatArray
+        flatArray.push(element2);
+      });
+    } else flatArray.push(element);
+  });
+  return flatArray;
 };
 
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
