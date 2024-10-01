b9ab5e14b01f5060877fba36396c50f582354dc3
connornlucas <connor_n_lucas@hotmail.com>
2024-10-01 11:13:39 -0700

    file created

---
+++ flatten.js
@@ -0,0 +1,20 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🟢 Assertion Passed: ${array1} and ${array2} are equal.`);
+  } else {
+    console.log(`🔻 Assertion Failed: ${array1} and ${array2} are not equal.`);
+  }
+};
+
