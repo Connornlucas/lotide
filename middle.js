22534ff3153f6e2c3de8736429683d72a6ffe7b0
connornlucas <connor_n_lucas@hotmail.com>
2024-10-01 12:51:39 -0700

    changed the variable i to midIndex as suggested

--- middle.js
+++ middle.js
@@ -35,11 +35,11 @@ const middle = function(array) {
   if (array.length <= 2) {
     return midArray;
   }
-  const i = Math.floor(array.length / 2);
+  const midIndex = Math.floor(array.length / 2);
   if (array.length % 2 === 0) {
-    midArray.push(array[i - 1], array[i]);
+    midArray.push(array[midIndex - 1], array[midIndex]);
   } else {
-    midArray.push(array[i]);
+    midArray.push(array[midIndex]);
   }
   return midArray;
 };
