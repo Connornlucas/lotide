95a50e11a951d52394f09155b22d9780f632e4de
connornlucas <connor_n_lucas@hotmail.com>
2024-10-01 18:09:56 -0700

    Linted

--- eqArrays.js
+++ eqArrays.js
@@ -7,17 +7,17 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
