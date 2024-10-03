65f4034e8effff1aa728ce33cade114ef677f6f8
connornlucas <connor_n_lucas@hotmail.com>
2024-10-03 10:43:20 -0700

    created file, working through problem

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔺 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// //Test
+// assertEqual('Lighthouse Labs', 'Bootcamp');
+// assertEqual(1, 1);
+// assertEqual("Connor", "Connor");
+
+const countOnly = function (allItems, itemsToCount) {};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
