c5eb0c60f80f0ec6f4c61bfeca975f41f5ef4c7a
connornlucas <connor_n_lucas@hotmail.com>
2024-10-03 11:10:30 -0700

    function working as intended

--- countOnly.js
+++ countOnly.js
@@ -11,7 +11,21 @@ const assertEqual = function(actual, expected) {
 // assertEqual(1, 1);
 // assertEqual("Connor", "Connor");
 
-const countOnly = function (allItems, itemsToCount) {};
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+     if (results[item]) {
+      results[item] += 1;
+     } else {
+      results[item] = 1;
+     }
+      console.log(item);
+    }}
+  
+  return results;
+};
 
 const firstNames = [
   "Karl",
