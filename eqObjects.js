4a32f057bebe6309a352f9e82a72c5e71cc98cce
connornlucas <connor_n_lucas@hotmail.com>
2024-10-07 15:23:25 -0700

    removed redundant key check, and modified line 29 for readability

--- eqObjects.js
+++ eqObjects.js
@@ -25,11 +25,8 @@ const eqObjects = function(object1, object2) {
   }
   const keys = Object.keys(object1);
   for (const key of keys) {
-    if (!(key in object2)) {
-      return false;
-    }
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (eqArrays(object1[key], object2[key]) === false) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else if (object1[key] !== object2[key]) {
