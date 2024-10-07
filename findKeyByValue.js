b6439344335360ccf8ce848de59f5f096d0767d0
connornlucas <connor_n_lucas@hotmail.com>
2024-10-07 13:48:10 -0700

    linted

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔺 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Test
+// assertEqual('Lighthouse Labs', 'Bootcamp');
+// assertEqual(1, 1);
+// assertEqual("Connor", "Connor");
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+// Tests
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
